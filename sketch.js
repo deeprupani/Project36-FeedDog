65fda996573780efe4ce30715063611bfd421181
deeprupani <75526959+deeprupani@users.noreply.github.com>
2021-04-07 11:14:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,119 @@
+var dog, sadDog, happyDog, database;
+var foodS, foodStock;
+var addFood;
+var foodObj;
+var getFoodStock, getFedTime;
+var deductFood;
+var feed, lastFed;
+var database;
+var fedTime;
+
+//create feed and lastFed variable here
+
+
+function preload() {
+  sadDog = loadImage("Dog.png");
+  happyDog = loadImage("happy dog.png");
+}
+
+function setup() {
+  createCanvas(1000, 400);
+  database = firebase.database();
+
+  foodObj = new Food();
+
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+
+  //write code to read fedtime value from the database 
+  fedTime = database.ref('FeedTime');
+  fedTime.on("value", setFedTime);
+
+  dog = createSprite(800, 200, 150, 150);
+  dog.addImage(sadDog);
+  dog.scale = 0.15;
+
+  //create feed the dog button here
+
+  feedthedog = createButton("Feed Dog");
+  feedthedog.position(700, 95);
+  feedthedog.mousePressed(deductFoods)
+
+  addFood = createButton("Add Food");
+  addFood.position(800, 95);
+  addFood.mousePressed(addFoods);
+
+}
+
+function draw() {
+  background(46, 139, 87);
+  foodObj.display();
+
+    //write code to display text lastFed time here
+    textSize(32);
+    fill("blue");
+    // if (lastFed >= 12) {
+    text("FedTime:" + fedTime, 200, 30);
+    // } else if (lastFed == 0) {
+    //   text("FedTime:" + hour() + ' : ' + minute(), 200, 30)
+    // } else {
+    //   text("FedTime:" + hour() + ' : ' + minute(), 200, 30)
+    // }
+  
+
+  drawSprites();
+}
+
+//function to read food Stock
+function readStock(data) {
+  foodS = data.val();
+  foodObj.updateFoodStock(foodS);
+}
+
+
+function feedDog() {
+  dog.addImage(happyDog);
+
+  //write code here to update food stock and last fed time
+  var food_stock_val = foodObj.getFoodStock();
+  if (food_stock_val <= 0) {
+    foodObj.updateFoodStock(food_stock_val * 0);
+  } else {
+    foodObj.updateFoodStock(food_stock_val - 1)
+  }
+  
+  updateFedTime();
+}
+
+//function to add food in stock
+function addFoods() {
+  foodS++;
+
+  database.ref('/').update({
+    Food: foodS
+  })
+}
+
+function deductFoods() {
+  
+  updateFedTime();
+  
+  foodS--;
+
+  database.ref('/').update({
+    Food: foodS
+  })
+}
+
+function setFedTime(data) {
+  fedTime = data.val()
+}
+
+function updateFedTime() {
+  fedTime = hour() + ' ' + minute();
+  console.log('updateFedTime => ', fedTime);
+  
+  database.ref('/').update({
+    FeedTime: fedTime
+  })
+}
